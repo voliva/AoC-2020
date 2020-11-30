7cf61a45f27d553d5b62e0d67e290273ba3a8f9a
Víctor Oliva <olivarra1@gmail.com>
2020-11-30 23:15:18 +0100

    add findShortestPath

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,5 @@
+import Queue from 'flatqueue';
+
 export const readGrid = (inputLines: string[]) =>
   inputLines.map(line => line.split(''));
 
@@ -15,3 +17,43 @@ export const regexParse = <T>(
   }
   return mapFn(result);
 };
+
+// Graph
+export function findShortestPath<T>(
+  root: T,
+  getEdges: (node: T, path: T[]) => (T | [T, number])[],
+  found: (node: T, path: T[]) => boolean,
+  getId: (value: T) => any = value => value
+) {
+  const visited = new Set<T>();
+  const tasks = new Queue();
+  tasks.push(
+    {
+      path: [root],
+      weight: 0,
+    },
+    0
+  );
+
+  let task;
+  while ((task = tasks.pop())) {
+    const {path, weight} = task;
+    const node: T = path[path.length - 1];
+    visited.add(node);
+    if (found(node, path)) return [node, path] as [T, T[]];
+
+    getEdges(node, path)
+      .map(edge => (Array.isArray(edge) ? edge : ([edge, 1] as const)))
+      .filter(([node]) => !visited.has(getId(node)))
+      .forEach(([node, edgeWeight]) =>
+        tasks.push(
+          {
+            path: [...path, node],
+            weight: weight + edgeWeight,
+          },
+          weight + edgeWeight
+        )
+      );
+  }
+  return null;
+}
